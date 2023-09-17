78c12388ae8f6f208ef322221d441102674d1b3b
Daniel Lewis <daniel@t14.t14>
2023-09-17 16:14:21 -0500

    beginning file selection

--- context_command.rb
+++ context_command.rb
@@ -1,9 +1,10 @@
 # Ruby requires
 require 'java'
 require 'json'
-
+require 'pathname'
 # Java imports
 java_import javax.swing.JTabbedPane
+java_import javax.swing.JFileChooser
 java_import javax.swing.JPanel
 java_import javax.swing.JTextArea
 java_import javax.swing.JLabel
@@ -30,19 +31,17 @@ java_import 'burp.ITab'
 java_import 'burp.IContextMenuFactory'
 
 
-
-
 class BurpExtender
   include IBurpExtender
   include ITab
   include IContextMenuFactory
   attr_reader :callbacks
   $num_tabs = 0
-  $commands = []
   $divder = '---------------------------------------------------------------------------------'
 
 
   def registerExtenderCallbacks(callbacks)
+    
     @callbacks = callbacks
     # Set Extension name
     @callbacks.setExtensionName('context command')
@@ -55,6 +54,7 @@ class BurpExtender
     @callbacks.addSuiteTab(self)
     # register a factory for custom context menu items.
     @callbacks.registerContextMenuFactory(self)
+    @output_file = ""
     # obtain our output and error streams
     stdout = java.io.PrintWriter.new callbacks.getStdout, true
     stderr = java.io.PrintWriter.new callbacks.getStderr, true
@@ -64,13 +64,18 @@ class BurpExtender
 
   def addPanel(tabs)
     # Implement Subtabs
-    
+    @command_records = {}
+
     #output subtab just needs a big text area. Maybe we will implement colors for
     #commands that support that in the future
-    @output = JPanel.new()
+    @outputcontainer = JPanel.new
+    layout = java.awt.BorderLayout.new
+    @outputcontainer.setLayout(layout)
+    
+    @output = JPanel.new
     layout       = java.awt.GridBagLayout.new
     @output.setLayout(layout)
-    tabs.addTab("Output", @output)
+    tabs.addTab("Output", @outputcontainer)
     constraints  = java.awt.GridBagConstraints.new
     constraints.anchor     = java.awt.GridBagConstraints::FIRST_LINE_START
     constraints.fill       = java.awt.GridBagConstraints::BOTH
@@ -94,13 +99,20 @@ class BurpExtender
     editorScrollPane = JScrollPane.new(@current_output);
     editorScrollPane.setVerticalScrollBarPolicy(
       JScrollPane::VERTICAL_SCROLLBAR_ALWAYS);
+
     
+    @output_button = JButton.new("Select Output File");
+    @output_button.addActionListener { selectFile }
+
     @output.add(editorScrollPane, constraints)
 
+    @outputcontainer.add(@output, 'Center')
+    @outputcontainer.add(@output_button, 'South')
+    
     #BEGIN COMMANDS SUBTAB
     #the commands subtab
     @commands = JPanel.new
-    layout       = java.awt.GridBagLayout.new()
+    layout       = java.awt.GridBagLayout.new
     @commandsButtonsContainer = JPanel.new(layout)
     @commandsButtonsContainer.setBackground(Color.black)
     layout       = java.awt.BorderLayout.new
@@ -108,17 +120,17 @@ class BurpExtender
     tabs.addTab("Commands", @commands)
     @commandScrollPane = JScrollPane.new(@commandsButtonsContainer);
     @commandScrollPane.setVerticalScrollBarPolicy(JScrollPane::VERTICAL_SCROLLBAR_ALWAYS);
-    @commandScrollPane.setHorizontalScrollBarPolicy(JScrollPane::HORIZONTAL_SCROLLBAR_NEVER);
+    #@commandScrollPane.setHorizontalScrollBarPolicy(JScrollPane::HORIZONTAL_SCROLLBAR_NEVER);
     @commands.add(@commandScrollPane,'Center')
 
     layout       = java.awt.GridLayout.new(0,2)
     @addsave = JPanel.new(layout)
-    
+
     @add_button = JButton.new("Add");
     @add_button.addActionListener { addCommand('','') }
 
     @save_button = JButton.new("Save");
-    @save_button.addActionListener { save() }    
+    @save_button.addActionListener { save }    
 
     @addsave.add(@add_button)
     @addsave.add(@save_button)
@@ -133,15 +145,33 @@ class BurpExtender
     @labels.add(label2, 'Center')
     @commands.add(@labels, 'North')
 
-
     @command_input = []
-
     
     path = File.expand_path(File.dirname(__FILE__))
     config = File.join(path, 'context_command.json')
-    
-    
-    addCommand('x', 'y')
+    #default number of slots
+    default = 8
+
+    if File.file?(config)
+      #open file
+      file = File.open(config)
+      data = File.read(file)
+      config_json = JSON.parse(data)
+      cnt = 0
+
+      config_json.each do |key, value|
+        addCommand(key, value)
+        cnt = cnt + 1        
+      end
+      rest = default - cnt
+      for x in 1..rest do
+        addCommand('','')
+      end
+    else
+      for x in 1..default do
+        addCommand('','')
+      end
+    end
 
     @commandsButtonsContainer.setBackground(Color.gray)
 
@@ -149,6 +179,36 @@ class BurpExtender
     tabs.addTab("Settings", @settings)
     @number_commands = 1
     # Add a Label in panel
+  end
+
+  def selectFile()
+    fchooser = JFileChooser.new
+    fchooser.set_dialog_title("Select where to save command output")
+    success = fchooser.show_open_dialog(nil)
+    if success == JFileChooser::APPROVE_OPTION
+      @output_file = Pathname.new(fchooser.get_selected_file.get_absolute_path)
+      @output_button.setText("Current output file: " + @output_file.to_s)
+    else
+      nil
+    end
+  end
+  
+  def updateCommands()
+    @command_records.clear
+    list = @commandsButtonsContainer.getComponents
+    for cmd in list
+      children = cmd.getComponents
+      if children[0].getText.to_s != '' and children[1].getText.to_s != ''
+        @command_records[children[0].getText.to_s] = children[1].getText.to_s
+      end
+    end
+  end
+
+  def save()   
+    path = File.expand_path(File.dirname(__FILE__))
+    config = File.join(path, 'context_command.json')
+    updateCommands
+    File.write(config, JSON.dump(@command_records))
   end
 
   def addCommand(label, exe)
@@ -175,10 +235,10 @@ class BurpExtender
     line.add(name,'West')
     line.add(command, 'Center')
     #remove command button
-    
+
     rmbutton = JButton.new("Remove");
     rmbutton.putClientProperty( "index", @command_input.length );
-    rmbutton.addActionListener { rmcommand(rmbutton, line)  }
+    rmbutton.addActionListener { rmcommand(rmbutton, line, @command_input.length)  }
     line.add(rmbutton, 'East')
     
     @command_input.push(line)
@@ -191,18 +251,25 @@ class BurpExtender
     constraints.gridx = 0
     constraints.gridy = @command_input.length()
     constraints.insets     = java.awt.Insets.new(5,5,5,5)
+    @command_input[-1].setPreferredSize(java.awt.Dimension.new(1,1))
     @commandsButtonsContainer.add(@command_input[-1], constraints)
-    @commandsButtonsContainer.revalidate()
-    @commandsButtonsContainer.repaint()
+    @commandsButtonsContainer.revalidate
+    @commandsButtonsContainer.repaint
+    updateCommands
   end
 
-  def rmcommand(button, line)
+  def rmcommand(button, line, index)
+    #@command_input.delete_at(index)
     @commandsButtonsContainer.remove(line)
-    @commandsButtonsContainer.revalidate()
-    @commandsButtonsContainer.repaint()
+    @commandsButtonsContainer.revalidate
+    @commandsButtonsContainer.repaint
+    save
+    puts @command_records
+
   end
   
   def createMenuItems(invocation)
+    updateCommands
     # Array of menus datastore
     menu_list = []
 
@@ -211,23 +278,25 @@ class BurpExtender
 
     # Create Section menu (contains sub-menus)
     sectionsMenu = JMenu.new('commands')
-    $commands.each{
+    @command_records.each_key{
+      |com|
       # Create a regular menuitem
-      |com| item = JMenuItem.new(com)
+      item = JMenuItem.new(com)
       # Add an action
-      item.addActionListener { execCommand com, invocation } 
+      item.addActionListener { execCommand @command_records[com], invocation } 
       # Add it to it's parent, the section menu
       sectionsMenu.add(item)
     }
-    
+
     # All menus has to be added to the array
     menu_list << menuItem
     menu_list << sectionsMenu
   end
 
   def execCommand(com, invocation)
+    puts com
     command = com.dup
-    request = invocation.getSelectedMessages()[0].getRequest()
+    request = invocation.getSelectedMessages[0].getRequest
     delim = '#'
     #pull the tag from between the delims get first elem of reuslting array and to string
     tags = command.split(delim, -1)[1...-1]
@@ -241,24 +310,26 @@ class BurpExtender
       elsif tag == "Body"
       else #tag is a header
         tag += ":"
-        lines = request.to_s.lines()
-        chunk = lines.select { |line| line =~ /^(#{tag})/ }
-        chunk = chunk[0].split(":")[1].strip()
+        lines = request.to_s.lines
+        chunk = lines.select { |line| line.downcase =~ /^(#{tag.downcase})/ }
+        chunk = chunk[0].split(":")[1].strip
         command.gsub!(original, chunk)
-        puts command 
       end
 
     }
-    # merge standard output and standard error using
-    # spawn option.  See the document of Kernel.spawn.
-    IO.popen([command, :err=>[:child, :out]]) {|ls_io|
-      ls_result_with_error = ls_io.read
-      current = @current_output.getText().to_s + "\n" + command + "\n"+ ls_result_with_error + $divder
-      @current_output.setText(current)        
+
+    #need to run the command in another thread so entire suite doesn't lock up on long cmds
+    thread = Thread.new {
+      IO.popen([command, :err=>[:child, :out]]) {|io| 
+        rdr = io.read
+        current = @current_output.getText.to_s + "\n" + command + "\n"+ rdr.to_s + "\n" + $divder
+        @current_output.setText(current)
+      }
+      #save output to a file since this isn't going to be read it should be safe in the thread
     }
   end
 
-  # ITab::getTabCaption()
+  # ITab::getTabCaption
   #
   # Set the tab caption
   def getTabCaption
